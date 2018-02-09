1d9301d5262195accae53bcda3902b092944fa61
WillOIP <w.pierlot@icloud.com>
2018-02-09 02:31:49 +0000

    Update

--- src/main/java/scooterise/combat/Creature.java
+++ src/main/java/scooterise/combat/Creature.java
@@ -1,15 +1,24 @@
 package scooterise.combat;
-
+import scooterise.combat.Weapon;
+import java.util.HashMap;
 public class Creature {
+    final class Attribute{
+        
+    }
    public int maxhp = 15;
+   public int hp = 15;
    public String CreatureName ="undefined";
    public boolean isPlayer = false;
    public boolean canBePlayer = false;
-   public class playerclass{
+   HashMap<Weapon,Boolean> weapons = new HashMap<Weapon,Boolean>();
+   public void attack(Creature enemy,Weapon weapon){
+      
+   }
+   final class playerclass{
        
    }
-   public class InvalidChracter extends Exception{
-       public InvalidChracter(int error){
+   public class InvalidChractor extends Exception{
+       public InvalidChractor(int error){
            switch(error){
                case 1:
                
